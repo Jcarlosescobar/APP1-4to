fd114fb75b1abc007cb2e563e4cbfc5c0fd64db8
Jcarlosescobar <85755807+Jcarlosescobar@users.noreply.github.com>
2021-08-09 16:41:51 -0600

    Add files via upload

---
+++ App 1 4to bimestre/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace App_1_4to_bimestre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string R = "s";
+            while (R == "s")
+            {
+                Console.WriteLine("Escribe el nombre del arhivo");
+                string archivo = Console.ReadLine();
+
+                StreamWriter ar;
+                ar = File.CreateText(archivo + ".txt");
+                ar.WriteLine("Archivo Creado");
+                ar.Close();
+
+                Console.WriteLine("Quieres crear otro archivo?");
+                Console.WriteLine("s/n");
+                R = Console.ReadLine();
+                Console.Clear();
+                if (R == "no")
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
